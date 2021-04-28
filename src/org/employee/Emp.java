ef17663a0a7711b4b4cef9276c1652c7fabbd5d5
SiddhuSathish7 <sportivesathish07@gmail.com>
2021-04-28 10:57:49 +0530

    task added

---
+++ src/org/employee/Emp.java
@@ -0,0 +1,37 @@
+package org.employee;
+
+public class Emp {
+	private void emplId() {
+		System.out.println("Employee id 4983");
+
+	}
+    private void emplName() {
+    	System.out.println("Employee name kavin");
+
+	}
+   private void emplDob() {
+	   System.out.println("Employee dob 16091994");
+
+}
+	private void emplPhn() {
+	System.out.println("Employee phn 9842424983");
+
+}
+	private void emplEmail() {
+	System.out.println("Employee email kavincool16");
+
+	}
+	private void emplAddr() {
+	System.out.println("Employee addr chennai");
+}
+	public static void main(String[] args) {
+		Emp z = new Emp();
+		z.emplId();
+		z.emplName();
+		z.emplDob();
+		z.emplPhn();
+		z.emplEmail();
+		z.emplAddr();
+	}
+}
+
---
+++ src/org/employee/Employee.java
@@ -0,0 +1,23 @@
+package org.employee;
+
+public class Employee {
+	public static void main(String[] args) {
+		byte age = 0;
+		System.out.println("Age is " + age);
+		short bikeNo = 3456;
+		System.out.println("Bike NO is "+ bikeNo);
+		int empId = 326378;
+		System.out.println("Employee Id is "+ empId);
+		long phoneNo = 9876543210l;
+		System.out.println("Phone no is "+ phoneNo);
+		char gender = 'M';
+		System.out.println("Gender is "+ gender);
+		String name = "Sathish";
+		System.out.println("Emp Name is "+ name);
+		float exp = 4.8f;
+		System.out.println("Emp exp is "+ exp);
+		String dob = null;
+		System.out.println("Emp DOB is " + dob);
+	}
+
+}
